a4fd1aaf93fc7181bc6993e0d83ca13565792ffe
German Garcia <german.garcia@bbva.ch>
2015-11-16 17:48:31 +0100

    refactor directive, fixed table data

--- app/app.module.js
+++ app/app.module.js
@@ -1,3 +1,3 @@
 'use strict';
 
-angular.module('ames-admin', ['ui.router', 'ngMdIcons', 'ngMaterial', 'amDate']);
+angular.module('ames-admin', ['ui.router', 'ngMdIcons', 'ngMaterial', 'ui-date', 'ui-table']);
